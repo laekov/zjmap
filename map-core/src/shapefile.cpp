35772d723bd2098e800c200c24139a152ee3e539
LazyJazz <zijian.lv@hotmail.com>
2022-04-01 15:14:41 +0800

    [update] minor update.

--- map-core/src/shapefile.cpp
+++ map-core/src/shapefile.cpp
@@ -103,3 +103,67 @@ std::vector <std::vector<SHPPOINT>> load_shapefile(const char *file_path) {
 
 }
 
+static const double pi = 3.14159265358979323846264338327950288419716939937510;
+static const double a = 6378245.0;
+static const double ee = 0.00669342162296594323;
+static const double bd_pi = 3.14159265358979324 * 3000.0 / 180.0;
+
+static double transformLat(double x, double y)
+{
+    double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y
+                 + 0.2 * sqrt(abs(x));
+    ret += (20.0 * sin(6.0 * x * pi) + 20.0 * sin(2.0 * x * pi)) * 2.0 / 3.0;
+    ret += (20.0 * sin(y * pi) + 40.0 * sin(y / 3.0 * pi)) * 2.0 / 3.0;
+    ret += (160.0 * sin(y / 12.0 * pi) + 320 * sin(y * pi / 30.0)) * 2.0 / 3.0;
+    return ret;
+}
+
+static double transformLon(double x, double y)
+{
+    double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * sqrt(abs(x));
+    ret += (20.0 * sin(6.0 * x * pi) + 20.0 * sin(2.0 * x * pi)) * 2.0 / 3.0;
+    ret += (20.0 * sin(x * pi) + 40.0 * sin(x / 3.0 * pi)) * 2.0 / 3.0;
+    ret += (150.0 * sin(x / 12.0 * pi) + 300.0 * sin(x / 30.0 * pi)) * 2.0 / 3.0;
+    return ret;
+}
+
+
+SHPPOINT WGS84_to_GCJ02(SHPPOINT coord) {
+    double dLat = transformLat(coord.X - 105.0, coord.Y - 35.0);
+    double dLon = transformLon(coord.X - 105.0, coord.Y - 35.0);
+    double radLat = coord.Y / 180.0 * pi;
+    double magic = sin(radLat);
+    magic = 1 - ee * magic * magic;
+    double sqrtMagic = sqrt(magic);
+    dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi);
+    dLon = (dLon * 180.0) / (a / sqrtMagic * cos(radLat) * pi);
+    double mgLat = coord.Y + dLat;
+    double mgLon = coord.X + dLon;
+    return SHPPOINT{mgLon,mgLat};
+}
+
+SHPPOINT GCJ02_to_WGS84(SHPPOINT coord) {
+    SHPPOINT d = WGS84_to_GCJ02(coord);
+    return SHPPOINT{coord.X * 2 - d.X, coord.Y * 2 - d.Y};
+}
+
+SHPPOINT GCJ02_to_BD09(SHPPOINT coord) {
+    double x = coord.X, y = coord.Y;
+    double z = sqrt(x * x + y * y) + 0.00002 * sin(y * bd_pi);
+    double theta = atan2(y, x) + 0.000003 * cos(x * bd_pi);
+    double bd_lon = z * cos(theta) + 0.0065;
+    double bd_lat = z * sin(theta) + 0.006;
+    return SHPPOINT{bd_lon, bd_lat};
+}
+
+SHPPOINT BD09_to_GCJ02(SHPPOINT coord) {
+    double x = coord.X - 0.0065, y = coord.Y - 0.006;
+    double z = sqrt(x * x + y * y) - 0.00002 * sin(y * bd_pi);
+    double theta = atan2(y, x) - 0.000003 * cos(x * bd_pi);
+    double gg_lon = z * cos(theta);
+    double gg_lat = z * sin(theta);
+    return SHPPOINT{gg_lon, gg_lat};
+}
+
+
+
